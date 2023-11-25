072a8fcd7c422d29dbfb741e2ecf24c651582f1e
nusaifpk <nusaifputhiyakath1@gmail.com>
2023-11-25 10:38:16 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/components/Child.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Child = () => {
+  return (
+    <div>
+        <h2 className='header'>Child Component of Home</h2>
+    </div>
+  )
+}
+
+export default Child
